fe4d4de1aa5ba5c377a36cde36c6cd47c14923a2
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-01-23 18:21:08 +0100

    Feat: Back Page Backend

--- Aptus/Exercises/BackPage.xaml.cs
+++ Aptus/Exercises/BackPage.xaml.cs
@@ -6,4 +6,38 @@ public partial class BackPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    void TapPullUp(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.PullUp());
+    }
+
+    void TapCableRow(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.CableRow());
+    }
+
+    void TapDeadlift(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.Deadlift());
+    }
+
+    void TapDumbellPullover(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.DumbellPullover());
+    }
+
+    void TapDumbellShrugs(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.DumbellShrugs());
+    }
+
+    void TapShrugs(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.Shrugs());
+    }
+
+    void TapStandingCableRow(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.StandingCableRow());
+    }
 }
