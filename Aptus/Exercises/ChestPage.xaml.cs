67c97c5ef7d2f87e1004b58726d8b7b745de3e38
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-01-23 18:05:40 +0100

    Feat: Chest Exercises Backend

--- Aptus/Exercises/ChestPage.xaml.cs
+++ Aptus/Exercises/ChestPage.xaml.cs
@@ -6,4 +6,43 @@ public partial class ChestPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    void TapBenchPress(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.BenchPress());
+    }
+    void TapInclineBenchPress(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.InclineBenchPress());
+    }
+
+    void TapDeclinePress(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.DeclinePress());
+    }
+
+    void TapDumbellFlyes(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.DumbellFlyes());
+    }
+
+    void TapSeatedDumbellFlyePress(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.SeatedDumbellFlyePress());
+    }
+
+    void TapInclineDumbellPress(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.InclineDumbellPress());
+    }
+
+    void TapCableFlye(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.CableFlye());
+    }
+
+    void TapStandingDumbelFlye(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Explanation.StandingDumbelFlye());
+    }
+
 }
